c9ecbe759b1c8d70d166d571436662cd5a5ef8d6
Errica <errica.cheng@gmail.com>
2020-02-27 22:03:09 -0500

    Added stats
    for basically everyone as of feb 27

--- src/MainProcessor.java
+++ src/MainProcessor.java
@@ -28,6 +28,7 @@ public class MainProcessor {
 	
     public static void main(String[] args) {
     	UnitInfo annette = new UnitInfo();
+    	UnitInfo ansehelm = new UnitInfo();
     	UnitInfo arvyn = new UnitInfo();
     	UnitInfo ashe = new UnitInfo();
     	UnitInfo bernadetta = new UnitInfo();
@@ -63,51 +64,52 @@ public class MainProcessor {
 		UnitInfo sylvain = new UnitInfo();
 		UnitInfo thyra = new UnitInfo();
 
-		UnitInfo[] unitInfoList = {annette, arvyn, ashe, bernadetta, briar, byleth, caleb, caspar, claude, cyril, dimitri,
+		UnitInfo[] unitInfoList = {annette, ansehelm, arvyn, ashe, bernadetta, briar, byleth, caleb, caspar, claude, cyril, dimitri,
 				dorothea, dedue, edelgard, emilie, felix, ferdinand, ghadir, hilda, hubert, ignatz, ingrid, layla, leonie,
 				linhardt, lorenz, lysithea, marianne, matilda, mercedes, petra, raphael, rowan, sylvain, thyra};
-		String[] unitNameList = {"Annette", "Arvyn", "Ashe", "Bernadetta", "Briar", "Byleth", "Caleb", "Caspar", "Claude", "Cyril", "Dimitri",
+		String[] unitNameList = {"Annette", "Ansehelm", "Arvyn", "Ashe", "Bernadetta", "Briar", "Byleth", "Caleb", "Caspar", "Claude", "Cyril", "Dimitri",
 				"Dorothea", "Dedue", "Edelgard", "Emilie", "Felix", "Ferdinand", "Ghadir", "Hilda", "Hubert", "Ignatz", "Ingrid", "Layla", "Leonie",
 				"Linhardt", "Lorenz", "Lysithea", "Marianne", "Matilda", "Mercedes", "Petra", "Raphael", "Rowan", "Sylvain", "Thyra"};
 		 
 
-		// name, hp, str, mag, def, res, dex, spd, lck, cha, magEqp (false = phys),
-		// mtBase, hitBase, critBase, wt
-		/*annette.instantiateUnit("Annette", );
-		arvyn.instantiateUnit("Arvyn", );
-		ashe.instantiateUnit("Ashe", );
+		// name, hp, str, mag, dex, spd, lck, def, res,
+		// magEqp (false = phys), mtBase, hitBase, critBase, wt
+		annette.instantiateUnit("Annette", 25, 6, 16, 7, 7, 6,7, 10, 6, true, 2, 100, 10, 2);
+		arvyn.instantiateUnit("Arvyn", 27, 13, 6, 9, 8, 7, 7, 6, 7, false, 5, 90, 0, 5);
+		ashe.instantiateUnit("Ashe", 25, 10,5, 10, 9, 8,9, 6, 8, false, 6, 85, 0, 5);
+		/*ansehelm.instantiateUnit();
 		bernadetta.instantiateUnit("Bernadetta", );*/
 		briar.instantiateUnit("Briar", 27, 13, 6, 9, 8, 8, 6, 6, 7, false, 5, 90, 0, 5);
 		byleth.instantiateUnit("Byleth", 27, 13, 6, 9, 8, 8, 6, 6, 7, false, 5, 90, 0, 5);
-		/*caleb.instantiateUnit("Caleb", );
-		caspar.instantiateUnit("Caspar", );*/
+		caleb.instantiateUnit("Caleb", 15, 10, 12, 7, 10, 9, 7, 10, 10, false, 5, 90, 0, 5);
+		/*caspar.instantiateUnit("Caspar", );*/
 		claude.instantiateUnit("Claude", 26, 11, 5, 8, 10, 8, 7, 5, 10, false, 6, 85, 0, 6);
-		/*cyril.instantiateUnit("Cyril", );
-		dimitri.instantiateUnit("Dimitri", );
-		dorothea.instantiateUnit("Dorothea", );
-		dedue.instantiateUnit("Dedue", );
-		edelgard.instantiateUnit("Edelgard", );
+		/*cyril.instantiateUnit("Cyril", );*/
+		dimitri.instantiateUnit("Dimitri", 28, 13, 4, 9, 9, 5, 8, 5, 9, false, 5, 90, 0, 5);
+		dorothea.instantiateUnit("Dorothea", 26, 5, 15, 6, 8, 6, 4, 10, 10, true, 4, 80, 5, 4);
+		dedue.instantiateUnit("Dedue", 30, 12, 4, 5, 9, 6, 12, 5, 10, false, 8, 70, 0, 7);
+		/*edelgard.instantiateUnit("Edelgard", );
 		emilie.instantiateUnit("Emilie", );
-		felix.instantiateUnit("Felix", );
-		ferdinand.instantiateUnit("Ferdinand", );*/
+		felix.instantiateUnit("Felix", );*/
+		ferdinand.instantiateUnit("Ferdinand", 28, 12, 5, 8, 8, 6, 14, 2, 7, false, 6, 80, 0, 6);
 		ghadir.instantiateUnit("Ghadir", 33, 17, 0, 13, 10, 9, 4, 4, 0, false, 5, 90, 0, 5);
-		/*hilda.instantiateUnit("Hilda", );
-		hubert.instantiateUnit("Hubert", );
-		ignatz.instantiateUnit("Ignatz", );
-		ingrid.instantiateUnit("Ingrid", );*/
+		hilda.instantiateUnit("Hilda", 29, 13, 5, 9, 9, 7, 6, 4, 8, false, 8, 70, 0, 7);
+		/*hubert.instantiateUnit("Hubert", );*/
+		ignatz.instantiateUnit("Ignatz", 25, 12, 5, 7, 8, 8, 10, 10, 4, false, 6, 85, 0, 6);
+		ingrid.instantiateUnit("Ingrid", 28, 8, 6, 8, 8, 8, 8, 8, 8, false, 6, 80, 0, 6);
 		layla.instantiateUnit("Layla", 23, 7, 12, 9, 9, 7, 5, 8, 10, true, 3, 90, 3, 0);
-		/*leonie.instantiateUnit("Leonie", );
-		linhardt.instantiateUnit("Linhardt", );
-		lorenz.instantiateUnit("Lorenz", );*/
+		/*leonie.instantiateUnit("Leonie", );*/
+		linhardt.instantiateUnit("Linhardt", 30, 5, 20, 6, 5, 17, 5, 13, 5, true, 1, 80, 0, 8);
+		/*lorenz.instantiateUnit("Lorenz", );*/
 		lysithea.instantiateUnit("Lysithea", 24, 5, 14, 8, 10, 6, 4, 10, 9, true, 5, 80, 0, 5);
-		/* marianne.instantiateUnit("Marianne", ); */
-		matilda.instantiateUnit("Matilda", 30, 12, 10, 10, 10, 5, 9, 9, 5, false, 5, 90, 0, 5);
+		marianne.instantiateUnit("Marianne", 25, 5, 16, 9, 7, 6, 6, 9, 7, true, 1, 80, 0, 8); 
+		matilda.instantiateUnit("Matilda", 30, 10, 8, 8, 10, 5, 7, 7, 5, false, 5, 90, 0, 5);
 		mercedes.instantiateUnit("Mercedes", 25, 6, 12, 6, 10, 5, 8, 10, 8, false, 3, 90, 0, 5);
 		/*petra.instantiateUnit("Petra", );
-		raphael.instantiateUnit("Rowan", );
+		raphael.instantiateUnit("Raphael", );
 		rowan.instantiateUnit("Rowan", );*/
 		sylvain.instantiateUnit("Sylvain", 27, 11, 8, 9, 8, 6, 7, 5, 9, false, 6, 80, 0, 6);
-		thyra.instantiateUnit("Thyra", 25, 14, 2, 9, 10, 5, 8, 8, 9, false, 6, 80, 0, 6);
+		thyra.instantiateUnit("Thyra", 25, 14, 2, 10, 8, 5, 9, 9, 8, false, 6, 80, 0, 6);
 		
 		SwingUtilities.invokeLater(() -> {
 			JPanel buttonPanel = new JPanel();
